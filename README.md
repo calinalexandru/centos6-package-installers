e91ef23d0e9a796ed59d280db8c44dcac85c3879
Alexandru Calin <calinalexandru1990@gmail.com>
2018-09-21 12:35:44 +0300

    php-installer: fixed prompt functions & added validator

--- README.md
+++ README.md
@@ -1,13 +1,23 @@
 CentOS6 popular packages installer:
 ==========================
 
-Run be root / use sudo
+Run as root / use sudo
 
 Install LAMP
 -----
-curl https://github.com/calinalexandru/centos6-package-installers/blob/master/install-lamp.sh | sh
+runs apache/mysql/php installers
 
-Install PHP 7.x
+    curl https://raw.githubusercontent.com/calinalexandru/centos6-package-installers/master/php/install-php.sh | sh
+
+Install PHP 6.x, 7.x
 -----
-curl https://raw.githubusercontent.com/calinalexandru/centos6-package-installers/master/php/install-php.sh| sh
+can specify version
 
+
+    
+    curl https://raw.githubusercontent.com/calinalexandru/centos6-package-installers/master/php/install-php.sh| sh
+    
+    Installing php...
+    Choose version(press enter for OS default)
+    Suggested: 7.0, 7.1, 7.2
+    :
--- php/install-php.sh
+++ php/install-php.sh
@@ -1,25 +1,52 @@
 #!/usr/bin/env bash
-
-# prompt user
-clear
 echo 'Installing php...'
-echo 'Choose version(press enter for OS default)'
-echo 'Suggested: 7.0, 7.1, 7.2'
-read -p ':' VERSION
+
+prompt_version_text() {
+    echo 'Choose version (press enter for default repo)'
+    echo '(eg: 5,6 | 7.1 | 7,2 | 73 | ... )'
+}
+
+prompt_version_input() {
+    read -p 'version:' VERSION
+    echo ${VERSION};
+}
+
+version_validate() {
+    echo "${1}" | sed "s/[0-9]//g"
+}
+
+version_digits() {
+    echo "${1}" | sed "s/[^0-9]//g"
+}
+
+
+while : ; do
+    prompt_version_text
+
+    VERSION=$(prompt_version_input)
+    VERSION_DIGITS=$(version_digits ${VERSION})
+
+    [[ "$VERSION" != "$VERSION_DIGITS" ]] || break
+
+    if [[ "$VERSION_DIGITS" = "" ]]; then
+        echo "Invalid version. Please use chars in this set [0-9\.\,]";
+    else
+        break
+    fi
+done
 
 # prepare packages
-if [ "$VERSION" != '' ]; then
+if [ "${VERSION}" = '' ]; then
     echo 'Script will install default php'
 else
-    VERSION_CLEAN= echo ${VERSION} | sed "s/[^0-9]//g"
-    echo "Version is ${VERSION_CLEAN}"
+    echo "Version is ${VERSION}"
     yum -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-6.noarch.rpm
     yum -y install http://rpms.remirepo.net/enterprise/remi-release-6.rpm
     yum -y install yum-utils
-    yum-config-manager --enable remi-php${VERSION_CLEAN}
+    yum-config-manager --enable remi-php${VERSION_DIGITS}
 fi
 
 # install packages & base modules
 yum -y install php php-mcrypt php-cli php-gd php-curl php-mysql php-ldap php-zip php-fileinfo
 echo php -v
-echo 'Goodbye! '
+echo 'Goodbye!'
