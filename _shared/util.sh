3c38ef9aea2e21132eeca272b42ec05005e92076
Alexandru Calin <calinalexandru1990@gmail.com>
2018-09-26 03:20:05 +0300

    now loggin to file

--- _shared/util.sh
+++ _shared/util.sh
@@ -6,11 +6,12 @@ source ./_shared/util/util.php.sh
 # TODO :: write output to file
 instr() {
     line="$1"
+    logFile="./php.vcs.log"
 
     if [ "$2" = "" ]; then
-        log cmd "" "$line" & $($line > /dev/null 2>&1) || log error "failed" & spinner
+        log cmd "" "$line" & $($line >> $logFile 2>&1) || log error "failed" & spinner
     else
-        log cmd "$2" "$line" & $($line > /dev/null 2>&1) || log error "failed" & spinner
+        log cmd "$2" "$line" & $($line >> $logFile 2>&1) || log error "failed" & spinner
     fi
 }
 
